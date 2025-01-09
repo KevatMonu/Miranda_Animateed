5728b6a95e2fbd8434bb92f32337575f8376a749
Monu Kenvat <monukevat3@gmail.com>
2025-01-09 10:05:09 +0530

    80% done

--- script.js
+++ script.js
@@ -6,3 +6,40 @@ var swiper = new Swiper(".mySwiper", {
   });
 
 
+
+  const circle = document.querySelector('.page4-right-bottom-circle');
+  const img = circle.querySelector('img');
+  const h1 = circle.querySelector('h1');
+
+
+
+  circle.addEventListener('mouseenter', () => {
+      gsap.to(h1, {
+          x: 100, 
+          opacity: 0,
+          duration: 0.,
+          ease: "power2.out",
+      });
+
+      gsap.to(img, {
+          x: 300, 
+          duration: 0.1,
+          ease: "power2.out",
+      });
+  });
+
+  circle.addEventListener('mouseleave', () => {
+      gsap.to(h1, {
+          x: 0, // Reset h1 position
+          opacity: 1,
+          duration: 0.1,
+          ease: "power2.in",
+      });
+
+      gsap.to(img, {
+          x: -155, // Reset img position
+          duration: 0.1,
+          ease: "power2.in",
+      });
+  });
+
